7e115fd81b8ed3c9effb4821f2635902888dff6f
m10a50836171 <88240842+m10a50836171@users.noreply.github.com>
2021-08-11 21:18:39 +0900

    Update home.js

--- js/home.js
+++ js/home.js
@@ -7,4 +7,22 @@ $(window).on('load', function(){
   });
 });
 
+function FixedAnime(){
+  var headerH = $('#header').outerHeight(true);
+  var scroll = $(window).scrollTop();
+  if (scroll >= headerH){
+    $('#header').addClass('fixed');
+  } else {
+    $('#header').removeClass('fixed');
+  }
+}
+
+$(window).scroll(function () {
+  FixedAnime();
+});
+
+$(window).on('load', function () {
+  FixedAnime();
+});
+
 
