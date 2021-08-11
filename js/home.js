05d74bf622c7ca340238644c7cc1d9d5209d4b54
m10a50836171 <88240842+m10a50836171@users.noreply.github.com>
2021-08-11 11:11:59 +0900

    Update home.js

--- js/home.js
+++ js/home.js
@@ -1,6 +1,6 @@
 $(window).on('load', function(){
   $("#splash_logo").delay(2000).fadeOut('slow');
-  $("#splash").delay(2000).fadeOut('slow', function(){
+  $("#splash").delay(1000).fadeOut('slow', function(){
       $('body').addClass('swipeOn');
   });
   $('.swipeBackground').on('animationend', function(){
